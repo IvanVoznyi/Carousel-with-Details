423410f3df20d16ca938d3fff53830bd027498eb
Ivan Voznyi <ivan.voznyi92@gmail.com>
2024-02-21 18:24:41 +0200

    Initial Commit

---
+++ Carousel_5/Carousel_5App.swift
@@ -0,0 +1,17 @@
+//
+//  Carousel_5App.swift
+//  Carousel_5
+//
+//  Created by Ivan Voznyi on 21.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct Carousel_5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
