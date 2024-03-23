ca9c7812c9100f3fc52c30658b1e0ed5a196c699
Ivan Voznyi <ivan.voznyi92@gmail.com>
2024-03-23 23:07:24 +0200

    add detail

--- Carousel_5/ContentView.swift
+++ Carousel_5/ContentView.swift
@@ -7,15 +7,91 @@
 
 import SwiftUI
 
+struct Card {
+    let color: Color
+    let title: String
+}
+
 struct ContentView: View {
+    @State private var isCardTapped = false
+    @State private var currentTripIndex = 0
+    @GestureState private var dragOffset: CGFloat = 0
+    let cards: [Card] = [
+        Card(color: .black, title: "1"),
+        Card(color: .red, title: "2"),
+        Card(color: .green, title: "3"),
+        Card(color: .yellow, title: "4"),
+        Card(color: .accentColor, title: "5"),
+        Card(color: .brown, title: "6"),
+    ]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            GeometryReader { outerView in
+                HStack(spacing: 0) {
+                    ForEach(cards.indices, id: \.self) { index in
+                        GeometryReader { innerView in
+                            RoundedRectangle(cornerRadius: 10)
+                                .fill(cards[index].color)
+                                .overlay {
+                                    Text(cards[index].title)
+                                        .foregroundStyle(.white)
+                                        .font(.system(size: 150))
+                                }
+                                .onTapGesture {
+                                    withAnimation(.interpolatingSpring(.bouncy, initialVelocity: 0.3)) {
+                                        isCardTapped = true
+                                    }
+                                }
+                        }
+                        .padding(.horizontal, isCardTapped ? 0 : 20)
+                        .frame(width: outerView.size.width, height: currentTripIndex == index ? (isCardTapped ? outerView.size.height : 450) : 400)
+                        .opacity(currentTripIndex == index ? 1.0 : 0.7)
+                    }
+                }
+                .frame(width: outerView.size.width, height: outerView.size.height, alignment: .leading)
+                .offset(x: -CGFloat(currentTripIndex) * outerView.size.width)
+                .offset(x: dragOffset)
+                .gesture(
+                    !isCardTapped ?
+                    DragGesture()
+                        .updating($dragOffset, body: { (value, state, transaction) in
+                            state = value.translation.width
+                        })
+                        .onEnded({ (value) in
+                            let threshold = outerView.size.width * 0.65
+                            var newIndex = Int(-value.translation.width/threshold) + currentTripIndex
+                            newIndex = min(max(newIndex, 0), cards.count-1)
+                            currentTripIndex = newIndex
+                        })
+                    : nil
+                )
+            }
+            .animation(.interpolatingSpring(.bouncy), value: dragOffset)
+            
+            GeometryReader { geometry in
+                if isCardTapped {
+                    DetailView(title: cards[currentTripIndex].title, color: cards[currentTripIndex].color)
+                        .padding(.top, geometry.safeAreaInsets  .top)
+                        .transition(.move(edge: .bottom))
+                        .ignoresSafeArea()
+                    
+                    Button(action: {
+                        withAnimation {
+                            isCardTapped = false
+                        }
+                    }, label: {
+                        Image(systemName: "xmark.circle.fill")
+                            .font(.system(size: 30))
+                            .foregroundStyle(.white)
+                            .opacity(0.7)
+                            .contentShape(Rectangle())
+                    })
+                    .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .topTrailing)
+                    .padding(.trailing)
+                }
+            }
         }
-        .padding()
     }
 }
 
--- Carousel_5/DetailView.swift
+++ Carousel_5/DetailView.swift
@@ -8,11 +8,38 @@
 import SwiftUI
 
 struct DetailView: View {
+    let title: String
+    let color: Color
+    
+    @State var items = 3...Int.random(in: 3...10)
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color(color)
+                .ignoresSafeArea()
+            GeometryReader { geometry in
+                VStack(alignment: .leading, spacing: 5) {
+                    Text(title)
+                        .font(.system(size: 50, design: .rounded))
+                        .fontWeight(.heavy)
+                        .padding(.leading, 20)
+                        .foregroundStyle(.white)
+                        .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+                    
+                    VStack(alignment: .leading, spacing: 15) {
+                        ForEach(items, id: \.self) { _ in
+                            RoundedRectangle(cornerRadius: 4)
+                                .fill(.white)
+                                .frame(width: CGFloat(Int.random(in: 200...Int(geometry.size.width - 25))), height: 30)
+                                .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+                        }
+                    }.padding()
+                }
+            }
+        }
     }
 }
 
 #Preview {
-    DetailView()
+    DetailView(title: "1", color: .yellow)
 }
