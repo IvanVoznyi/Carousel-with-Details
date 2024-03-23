15ffac786d797d7171449645d4470f99a25f935d
Ivan Voznyi <ivan.voznyi92@gmail.com>
2024-03-23 23:06:35 +0200

    add detailView

---
+++ Carousel_5/DetailView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailView.swift
+//  Carousel_5
+//
+//  Created by Ivan Voznyi on 21.02.2024.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailView()
+}
